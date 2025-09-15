79876fa2ad29d0bddba9a98d32034156aba0826b
sadaka2025 <cformation75@gmail.com>
2025-09-15 04:23:09 +0100

    f

--- src/pages/Home/LayoutConfigurable.jsx
+++ src/pages/Home/LayoutConfigurable.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import { useTranslation } from 'react-i18next';
+import ChatWidget from '../../components/ChatWidget'; // <-- import du widget
 
 export default function LayoutConfigurable({ showNavbar = true }) {
   const { i18n } = useTranslation();
@@ -16,6 +17,8 @@ export default function LayoutConfigurable({ showNavbar = true }) {
       <main className="flex-grow w-full">
         <Outlet />
       </main>
+      {/* Widget flottant visible sur toutes les pages */}
+      <ChatWidget />
     </div>
   );
 }
